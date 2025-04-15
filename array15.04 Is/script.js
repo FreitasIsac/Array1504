d198760a2e54f11fe7599fe96df1d70434f81000
FreitasIsac <166455554+FreitasIsac@users.noreply.github.com>
2025-04-15 19:29:29 -0300

    Add files via upload

---
+++ array15.04 Is/script.js
@@ -0,0 +1,38 @@
+/* var num1 = 6
+var num2 = 7
+
+function soma(){
+    var soma = num1 + num2
+    alert (soma)
+}
+
+soma() */
+
+/* var peso = Number(prompt("Digite seu peso:"))
+var altura = Number(prompt("Digite sua altura:"))
+
+function imc(peso, altura){
+    return (peso / (altura * altura)).toFixed(2)
+}
+
+var resultado = imc(peso, altura)
+
+alert(resultado) */
+
+/* let nome = "Isac"          
+
+let nome = "Abraão"
+
+alert(nome) */
+
+function teste(){
+    if(true) {
+        var a = 10;
+        let resultado = 20;
+        console.log(resultado)
+    }
+     let resultado = 30
+    console.log(a)
+    console.log(resultado)
+}
+teste()
